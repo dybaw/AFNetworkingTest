bb93abe1259ed2b61a75b5233535c37ebd35d746
robert722 <robert1990@foxmail.com>
2015-08-21 16:27:57 +0800

    Initial Commit

---
+++ AFNetworkingTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AFNetworkingTest
+//
+//  Created by robert on 15/8/21.
+//  Copyright (c) 2015年 lyp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AFNetworkingTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AFNetworkingTest
+//
+//  Created by robert on 15/8/21.
+//  Copyright (c) 2015年 lyp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
